7c29fd54f68be51e118b0cb3feae12b1f2e7da55
pub@wordncodde.com <pub@wordncode.com>
2026-06-15 00:25:18 +0900

    빌드 에러 수정

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: 'export',
+  basePath: '/portfolio_2026',
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
